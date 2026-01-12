fd183f19764c7d718755b63d931fe7dc25c271d6
David Stansby <dstansby@gmail.com>
2026-01-12 22:52:01 +0000

    Load labels

--- src/ome_zarrpari/_widget.py
+++ src/ome_zarrpari/_widget.py
@@ -1,6 +1,8 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
 import ome_zarr_models.v04
+import ome_zarr_models.v04.multiscales
+import ome_zarr_models.v05.multiscales
 import zarr
 from ome_zarr_models import open_ome_zarr
 from qtpy.QtWidgets import (
@@ -15,7 +17,10 @@ if TYPE_CHECKING:
 
 from qtpy.QtWidgets import QLabel
 
-SUPPORTED_CLASSES = (ome_zarr_models.v05.Image,)
+SUPPORTED_CLASSES = (
+    ome_zarr_models.v04.Image,
+    ome_zarr_models.v05.Image,
+)
 
 
 class OMEZarrpariWidget(QWidget):
@@ -76,6 +81,7 @@ class OMEZarrpariWidget(QWidget):
             return
 
         self._load_ome_zarr_image(group, data, visible=visible)
+        self.status_text.setText("Successfully loaded")
 
     def _load_ome_zarr_image(
         self,
@@ -87,18 +93,65 @@ class OMEZarrpariWidget(QWidget):
         """
         Load an OME-Zarr image on to the napari viewer.
         """
-        for multiscale in image.ome_attributes.multiscales:
-            arrays = []
-            for dataset in multiscale.datasets:
-                array_store = zarr_group.store_path / dataset.path
-                arrays.append(
-                    zarr.open_array(
-                        array_store,
-                        zarr_format=2 if image.ome_zarr_version == 0.4 else 3,
-                    )
-                )
+        # Add all the images
 
+        for multiscale in image.ome_attributes.multiscales:
+            self._add_multiscale_layer(
+                multiscale, zarr_group, layer_type="image", visible=visible
+            )
+
+        # Check for labels
+        if (labels := image.labels) is not None:
+            for path in labels.ome_attributes.labels:
+                image_label_group = zarr.open_group(
+                    zarr_group.store_path / "labels" / path
+                )
+                image_labels = ome_zarr_models.v04.ImageLabel.from_zarr(
+                    image_label_group
+                )
+                for multiscale in image_labels.ome_attributes.multiscales:
+                    # TODO: correctly assign color from the label metdaata
+                    self._add_multiscale_layer(
+                        multiscale,
+                        image_label_group,
+                        layer_type="labels",
+                        visible=visible,
+                    )
+
+    def _add_multiscale_layer(
+        self,
+        multiscale: (
+            ome_zarr_models.v04.multiscales.Multiscale
+            | ome_zarr_models.v05.multiscales.Multiscale
+        ),
+        zarr_group: zarr.Group,
+        *,
+        layer_type: Literal["image", "labels"],
+        visible: bool = True,
+    ) -> None:
+        arrays = []
+        for dataset in multiscale.datasets:
+            array_store = zarr_group.store_path / dataset.path
+            arrays.append(
+                zarr.open_array(
+                    array_store,
+                    zarr_format=(
+                        2
+                        if isinstance(
+                            multiscale,
+                            ome_zarr_models.v04.multiscales.Multiscale,
+                        )
+                        else 3
+                    ),
+                )
+            )
+
+        # TODO: pass axis labels down
+        if layer_type == "image":
             self.viewer.add_image(
                 arrays, name=multiscale.name, multiscale=True, visible=visible
             )
-        self.status_text.setText("Successfully loaded")
+        elif layer_type == "labels":
+            self.viewer.add_labels(
+                arrays, name=multiscale.name, multiscale=True, visible=visible
+            )

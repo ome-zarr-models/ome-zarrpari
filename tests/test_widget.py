ff95f551b53ffbbed62ea1840190ca8d691a6d7a
David Stansby <dstansby@gmail.com>
2026-01-13 22:25:27 +0000

    Test on data with channels

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -8,17 +8,24 @@ from ome_zarrpari._widget import OMEZarrpariWidget
 def test_example_q_widget(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
 
     # create our widget, passing in the viewer
     widget = OMEZarrpariWidget(viewer)
     assert widget.load_pane_status_text == ""
     # Load an image
-    widget._load_ome_zarr(
-        "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.5/idr0066/ExpD_chicken_embryo_MIP.ome.zarr",
-        visible=False,
-    )
+    with pytest.warns(UserWarning, match="zarr array cannot be sliced lazily"):
+        widget._load_ome_zarr(
+            "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0062A/6001247.zarr",
+            visible=False,
+        )
     assert widget.load_pane_status_text == "Successfully loaded"
+
+    # Try a non-existing path
+    widget._load_ome_zarr("/non/existent/path", visible=False)
+    assert (
+        widget.load_pane_status_text
+        == "Loading OME-Zarr failed. See console for more details"
+    )
 
     # read captured output and check that it's as we expected
     # captured = capsys.readouterr()
